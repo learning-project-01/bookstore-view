200bc22b8052c10c7df94791288e365a14837df1
rabindrapatra <rabindrapatra@corp.com>
2024-02-09 23:51:53 +0530

    added media component

--- src/components/catalog/CatalogForm.jsx
+++ src/components/catalog/CatalogForm.jsx
@@ -12,9 +12,91 @@ import {
   Alert,
 } from "reactstrap";
 import { post } from "../../clients/HttpClient";
-import { APP_PROPS } from "../../constants/AppConstants";
+
+export function CatalogMedia({ index, deleteFunc }) {
+  const [media, setMedia] = useState({
+    index: index,
+    url: "",
+    format: "",
+    type: "",
+  });
+  const [edit, setEdit] = useState(false);
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    console.log(media);
+    setMedia({ ...media, [name]: value });
+    setEdit(true);
+  };
+  const handleSave = (e) => {
+    setEdit(false);
+  };
+  return (
+    <Row>
+      <Col md="3">
+        <FormGroup>
+          <Label for="name">URL</Label>
+          <Input
+            type="text"
+            name="url"
+            id="url"
+            value={media.url}
+            onChange={onChange}
+          />
+        </FormGroup>
+      </Col>
+      <Col md="3">
+        <FormGroup>
+          <Label for="name">Format</Label>
+          <Input
+            type="text"
+            name="format"
+            id="format"
+            value={media.format}
+            onChange={onChange}
+          />
+        </FormGroup>
+      </Col>
+      <Col md="3">
+        <FormGroup>
+          <Label for="name">Type</Label>
+          <Input
+            type="text"
+            name="type"
+            id="type"
+            value={media.type}
+            onChange={onChange}
+          />
+        </FormGroup>
+      </Col>
+      <Col md="3">
+        <FormGroup>
+          <Button
+            size="sm"
+            color="danger"
+            onClick={() => {
+              deleteFunc(index);
+            }}
+          >
+            Delete
+          </Button>
+
+          {edit && (
+            <Button size="sm" color="primary" onClick={handleSave}>
+              Save
+            </Button>
+          )}
+        </FormGroup>
+      </Col>
+    </Row>
+  );
+}
+
+const showMediaComponent = (mediaComponent, index) => (
+  <div key={index}>{mediaComponent}</div>
+);
 
 function CatalogForm() {
+  const [mediaComponents, setMediaComponents] = useState([]);
   // State for form fields
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -22,6 +104,26 @@ function CatalogForm() {
 
   // State for global error message
   const [errorMessage, setErrorMessage] = useState("");
+
+  const handleAddMedia = (e) => {
+    console.log(e, mediaComponents.length);
+    setMediaComponents([
+      ...mediaComponents,
+      <CatalogMedia
+        key={mediaComponents.length}
+        index={new Date().getTime()}
+        deleteFunc={deleteMedia}
+      />,
+    ]);
+  };
+
+  const deleteMedia = (indexOfMedia) => {
+    setMediaComponents((prevItems) =>
+      prevItems.filter((item) => {
+        return item.props.index !== indexOfMedia;
+      })
+    );
+  };
 
   // Function to handle form submission
   const handleSubmit = (e) => {
@@ -49,7 +151,7 @@ function CatalogForm() {
     <>
       <Container className="mt-5">
         <Row>
-          <Col md={{ size: 6, offset: 3 }}>
+          <Col md={{ size: 12 }}>
             <h3>Catalog Form</h3>
             <Form onSubmit={handleSubmit}>
               {/* Global Error Message */}
@@ -91,6 +193,9 @@ function CatalogForm() {
               <Button color="primary" type="submit">
                 Submit
               </Button>
+
+              <Button onClick={handleAddMedia}>Add Media</Button>
+              {mediaComponents.map(showMediaComponent)}
             </Form>
           </Col>
         </Row>
