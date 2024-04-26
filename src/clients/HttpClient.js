8ebfd6741d2579219a46827923d8d45d063dd63c
rabindra patra <docs.rabindrapatra@gmail.com>
2024-04-26 18:04:26 +0530

    Update HttpClient.js

--- src/clients/HttpClient.js
+++ src/clients/HttpClient.js
@@ -6,9 +6,17 @@ const headers = {
     'Content-Type': 'application/json', // Specify the content type if needed
   };
 
+function getHeaders(){
+    let  requestHeaders = {...headers};
+    if(localStorage.getItem('token')){
+  requestHeaders = {'X-AUTH-TOKEN': localStorage.getItem('token'), ...headers}
+  }
+    console.log('headers: ', requestHeaders);
+    return requestHeaders;
+}
 export function post(url, data, onSuccess, onError) {
   axios
-    .post(url, data, {headers})
+    .post(url, data, {getHeaders()})
     .then((response) => {
       if (onSuccess) {
         onSuccess(response);
@@ -25,7 +33,7 @@ export function post(url, data, onSuccess, onError) {
 }
 
 export function get(url, onSuccess, onError){
-    axios.get(url, {headers})
+    axios.get(url, {getHeaders()})
     .then(response=>{
         if(onSuccess){
             onSuccess(response)
