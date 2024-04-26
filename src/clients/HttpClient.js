d0540c67f5d6e72e63fe8c506c8bf0102113f4ce
rabindrapatra <rabindrapatra@corp.com>
2024-04-27 01:03:33 +0530

    redirect to login on 401 error

--- src/clients/HttpClient.js
+++ src/clients/HttpClient.js
@@ -6,43 +6,61 @@ const headers = {
     'Content-Type': 'application/json', // Specify the content type if needed
   };
 
-function getHeaders(){
-    let  requestHeaders = {...headers};
-    if(localStorage.getItem('token')){
-  requestHeaders = {'X-AUTH-TOKEN': localStorage.getItem('token'), ...headers}
+function getHeaders() {
+  let requestHeaders = { ...headers };
+  if (localStorage.getItem("token")) {
+    requestHeaders = {
+      "X-AUTH-TOKEN": localStorage.getItem("token"),
+      ...headers,
+    };
   }
-    console.log('headers: ', requestHeaders);
-    return requestHeaders;
+  console.log("headers: ", requestHeaders);
+  return requestHeaders;
 }
-export function post(url, data, onSuccess, onError) {
+
+function invoke(method, url, data, onSuccess, onError){
   const requestHeaders = getHeaders();
   console.log('requestHeaders: ', requestHeaders);
-  axios
-    .post(url, data, {headers:requestHeaders})
-    .then((response) => {
+
+  let promise = null;
+
+  switch(method){
+    case "post":
+      promise = axios.post(url, data, {headers:requestHeaders})
+      break
+    case "get":
+      promise = axios.get(url, {headers: requestHeaders})
+      break
+    default:
+      console.log(`http method ${method} implementation not found.`);  
+  }
+
+  if(promise){
+    promise
+    .then(response=>{
       if (onSuccess) {
         onSuccess(response);
       }
       console.log("Response:", response.data);
     })
-    .catch((error) => {
-      // Handle errors
+    .catch(error=>{
+      if(error?.response?.status===401){
+        window.location.href="/login"
+        return;
+      }
       if (onError) {
         onError(error);
       }
+      console.log({...error});
       console.error("Error:", error);
-    });
+    })
+  }
+}
+
+export function post(url, data, onSuccess, onError) {
+  invoke("post", url, data, onSuccess, onError);
 }
 
 export function get(url, onSuccess, onError){
-    axios.get(url, {headers:getHeaders()})
-    .then(response=>{
-        if(onSuccess){
-            onSuccess(response)
-        }
-    }).catch(error=>{
-        if(onError){
-            onError(error)
-        }
-    })
+  invoke("get", url, {}, onSuccess, onError);
 }
